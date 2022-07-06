2c5acec9c24b5b1fa04acb9c136481beaeb9bca3
Edwin <edwin.yi.128@gmail.com>
2022-07-05 20:00:21 -0500

    More JS

--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
+//Tamagotchi Class
+class Tamagotchi {
+    
+}
+
+//Game Class
+class Game extends Tamagotchi {
+
+}
+
+//Stats Intervals
+function hungerInterval () {
+}
+function sleepinessInterval () {
+
+}
+function boredomInterval () {
+
+}
+
 //Button Functions
 function startButton () {
-
+    let homescreen = document.querySelector(".homescreen")
+    homescreen.style.display = "none"
+    setInterval(hungerInterval, 1000)
 }
 function feedButton () {
 
@@ -22,3 +44,6 @@ function sleepinessCounter () {
 function boredomCounter () {
 
 }
+
+//Event Listeners
+document.querySelector("#start").addEventListener("click", startButton)
