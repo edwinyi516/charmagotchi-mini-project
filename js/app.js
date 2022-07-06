d13cc46cd81cd68f903023069698c98c69684314
Edwin <edwin.yi.128@gmail.com>
2022-07-05 20:13:59 -0500

    Rearranged JS to nest inside classes

--- js/app.js
+++ js/app.js
@@ -1,48 +1,49 @@
 //Tamagotchi Class
 class Tamagotchi {
-    
+    constructor(name, hunger, sleepiness, boredom) {
+        this.name = name
+        this.hunger = hunger
+        this.sleepiness = sleepiness
+        this.boredom = boredom
+    }
+    //Stats Intervals
+    hungerInterval () {
+
+    }
+    sleepinessInterval () {
+
+    }
+    boredomInterval () {
+
+    }
 }
+
+//Instantiate Tamagotchi
+const character = new Tamagotchi("", 0, 0, 0)
 
 //Game Class
-class Game extends Tamagotchi {
+class Game extends Tamagotchi{
+    constructor(name, hunger, sleepiness, boredom) {
+        super(name, hunger, sleepiness, boredom)
+        this.name = name
+        this.hunger = hunger
+        this.sleepiness = sleepiness
+        this.boredom = boredom
+    }
+    //Button Functions
+    startButton () {
+        let homescreen = document.querySelector(".homescreen")
+        homescreen.style.display = "none"
+    }
+    feedButton () {
 
-}
+    }
+    sleepButton () {
 
-//Stats Intervals
-function hungerInterval () {
-}
-function sleepinessInterval () {
+    }
+    playButton () {
 
-}
-function boredomInterval () {
-
-}
-
-//Button Functions
-function startButton () {
-    let homescreen = document.querySelector(".homescreen")
-    homescreen.style.display = "none"
-    setInterval(hungerInterval, 1000)
-}
-function feedButton () {
-
-}
-function sleepButton () {
-
-}
-function playButton () {
-
-}
-
-//Stats Counter
-function hungerCounter () {
-
-}
-function sleepinessCounter () {
-
-}
-function boredomCounter () {
-
+    }
 }
 
 //Event Listeners
